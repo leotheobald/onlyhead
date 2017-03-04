221549fb10844d5f8b2fa7647d9340dd13a6033a
Leo Theobald <leotheo@gmail.com>
2017-03-04 14:48:41 +0000

    test works in phantom

--- assets/scripts/write.js
+++ assets/scripts/write.js
@@ -1,6 +1,12 @@
 var text = require('./text');
+var date = require('./date');
+
 module.exports = function () {
     var heading = document.createElement('h2');
+    var spanTag = document.createElement('span');
+
+    spanTag.textContent = date;
     heading.textContent = text;
     document.getElementById('container').appendChild(heading);
+    document.getElementById('container').appendChild(spanTag);
 };
--- test/write.test.js
+++ test/write.test.js
@@ -1,6 +1,9 @@
 var write = require('../assets/scripts/write');
 var text = require('../assets/scripts/text');
+var today = require('../assets/scripts/date');
+
 var assert = require('assert');
+
 
 describe('write', function () {
     var div;
@@ -21,10 +24,25 @@ describe('write', function () {
         assert(el, 'el exists');
     });
 
+    it('Adds a span to h2', function () {
+        write();
+        var el = div.querySelector('span');
+        assert(el, 'el exists');
+    });
+
     it('Sets text to text.js module', function () {
         write();
         var el = div.querySelector('h2');
         assert(el.textContent === text, 'el text is ' + text);
+    });
+
+    it('Checks if date and format are correct and exist', function () {
+        write();
+
+        var el = div.querySelector('span');
+
+        console.log(today, el.textContent);
+        assert(el.textContent === today);
     });
 
 });
